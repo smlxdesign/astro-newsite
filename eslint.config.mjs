426d8a416e0b2cf6372d7a37c776736e22bd3d00
SMLX Design <smlxdesign@duck.com>
2025-02-03 20:48:17 +0100

    Add Husky pre-commit hook, and tailwind plugin for prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,31 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginAstro from "eslint-plugin-astro";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+	{
+		ignores: [
+			"node_modules/",
+			"dist/",
+			"build/",
+			".vscode/",
+			".idea/",
+			".astro/",
+			".git/",
+		],
+	},
 	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
 	pluginJs.configs.recommended,
+	...eslintPluginAstro.configs.recommended,
 	{
 		rules: {
 			"no-console": "warn",
 			"no-unused-vars": "warn",
 			"no-undef": "error",
 			eqeqeq: "error",
+			"no-var": "error",
 		},
 	},
 	eslintConfigPrettier,
